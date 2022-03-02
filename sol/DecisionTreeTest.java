8ca30f554f46492fc4a92c170cf746a70d977f70
angie <angela_osei-ampadu@brown.edu>
2022-03-02 10:54:44 -0500

    finished getDecision and generate Tree now working on testing

--- sol/DecisionTreeTest.java
+++ sol/DecisionTreeTest.java
@@ -9,17 +9,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 
+/*
+visualizer hasn't been working for me so working on writing tests in this class using parse and the csv files
+currently tryng to test methods on  a smaller set of dataset called fruittesting
+ */
 public class DecisionTreeTest {
-    
+    //make your own data set in the same csv format and call generateTree on
     // Constructor for DecisionTreeTest tester class - don't need to add anything in here!
+    //parse("/Users/angie/Desktop/cs200/decision-tree-icebear200-poseydurling/sol/fruittesting.csv")
     public DecisionTreeTest() {
-
     }
 
-//    @Test public void testTreeGeneration(){
-//        Assert.assertEquals();
-//    }
-    
     @Test
     public void testExample() { 
         assertEquals(6, 1 + 2 + 3);
--- sol/TreeGenerator.java
+++ sol/TreeGenerator.java
@@ -11,7 +11,7 @@ import java.util.List;
  * used to generate a tree
  */
 public class TreeGenerator implements ITreeGenerator<Dataset> {
-
+    public ITreeNode fullTree; // this is instance variable that represents the fullTree we create once we run generateTree
     public TreeGenerator(){
         //this.generateTree();
     }
@@ -31,13 +31,18 @@ public class TreeGenerator implements ITreeGenerator<Dataset> {
             for(Dataset data : trainingData.partition(splitAttribute)){
                 edgeList.add(new Edge(this.generateTreeHelper(data), data.getValue(splitAttribute)));
             }
-            return new AttributeNode(splitAttribute, edgeList, trainingData.getDefault(splitAttribute));
+            fullTree = new AttributeNode(splitAttribute, edgeList, trainingData.getDefault(splitAttribute));
+            return fullTree;
         }
     }
 
     @Override
     public String getDecision(Row datum) {
-        return null;
+      return  fullTree.getDecision(datum); //we call getDecision from the AttributeNode class to get the decision
+        // once we've generated the tree
     }
     // TODO: Implement methods declared in ITreeGenerator interface!
+
 }
+
+
